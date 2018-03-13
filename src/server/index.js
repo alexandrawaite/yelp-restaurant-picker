a5df4ab9f50e2e909a42f081e9ae550d4f358f61
alexandrawaite <alexandrawaite@gmail.com>
2018-03-13 14:12:53 -0700

    Write route that utilizes our Yelp API upon get request to /yelp/search/

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const YelpApi = require('./yelp_api')
 
 app.use(express.static('public'))
+
+// routes to access Yelp's API endpoints
+app.get('/yelp/search', (req, res) => {
+  let params = req.query
+
+  YelpApi.search(params)
+    .then(results => {
+      res.set('Access-Control-Allow-Origin', '*')
+      res.json(results.data)
+    })
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
