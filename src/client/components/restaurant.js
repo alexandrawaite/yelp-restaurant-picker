93873d49ebd68043913961e29926a631537d74f3
alexandrawaite <alexandrawaite@gmail.com>
2018-03-13 14:21:10 -0700

    Revise Restaurant component to connect with Yelp API restaurant result data

--- src/client/components/restaurant.js
+++ src/client/components/restaurant.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 
 export default class Restaurant extends Component {
   render() {
-    const { name, rating, location, cost } = this.props
+    const { name, rating, location, cost } = this.props.restaurant
     return (
       <div>
         <p>Name: {name}</p>
         <p>Rating: {rating}</p>
-        <p>Location: {location}</p>
+        <p>Location: {location.city}</p>
         <p>Cost: {cost}</p>
       </div>
     )
