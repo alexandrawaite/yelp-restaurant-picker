9646682a1f2319b30f0514682638de702ef03c18
alexandrawaite <alexandrawaite@gmail.com>
2018-03-13 14:23:19 -0700

    Add accept and reject restaurant buttons

--- src/client/components/restaurant_select_buttons.js
+++ src/client/components/restaurant_select_buttons.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class RestaurantSelectButtons extends Component {
   render() {
     return (
-      <div>This is the RestaurantSelectButtons component</div>
+      <div>
+        <button onClick={this.props.onRestaurantAccept}>Accept!</button>
+        <button onClick={this.props.onRestaurantReject}>Reject!</button>
+      </div>
     )
   }
 }
